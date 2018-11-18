6a9268aae330010fcfabe801e698893a17e5389b
Wyatt Duling <Wyatt.duling@gmail.com>
2018-11-18 15:49:34 -0800

    Board
    Took forever to construct the damn board lol

---
+++ Assets/Resources/Scripts/BoardSpace.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardSpace : MonoBehaviour {
+
+	public SpaceSelection SpaceType;
+
+	public enum SpaceSelection
+	{
+		Black, 
+		White
+	};
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
