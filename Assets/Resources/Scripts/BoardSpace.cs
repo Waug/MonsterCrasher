ccb010899b6f0edd5438f1321fcfbc3668f1dbbb
Wyatt Duling <Wyatt.duling@gmail.com>
2018-11-20 18:11:29 -0800

    Creature is now capable of moving
    A creature with a home space can hand a movement option to its homespace. It will receive a set of spaces to move into and then will do so.

--- Assets/Resources/Scripts/BoardSpace.cs
+++ Assets/Resources/Scripts/BoardSpace.cs
@@ -3,87 +3,91 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoardSpace : MonoBehaviour {
-
+	
 	public bool SQUARETEST;
 	public Movement.MOVEMENT_NOTATION NOTATIONTEST;
 	public Monster.MonsterOrientation ORITEST;
-
+	
 	public SpaceSelection SpaceType;
 	private string pointerSpace;
-
+	
 	private string[] boardLetters 				= 	{"AA", "A", "B", "C", "D", "E", "F", "G", "H", "HH"};
 	private int[] boardNumbers 					= 	{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	private int[] vectorIndex 	= new int[2];
 	private int[,] orientationIndex			 	= new int[,] {{-1,-1}, {0,1}, {1,-1}, {-1,0}, {1,1}, {0,-1}, {-1,1}, {1,0}};
 	//{  	
-//								new int[,] 	{{-1,-1}, {0,1}, {1,-1}, {-1,0}, {1,1}, {0,-1}, {-1,1}, {1,0}},
-								//new int[,] 	{{-1,0}, {0,-1}, {0,-1}, {1,0}, {1,0}, {0,1}, {0,1}, {-1,0}}
-//	};
-
+	//								new int[,] 	{{-1,-1}, {0,1}, {1,-1}, {-1,0}, {1,1}, {0,-1}, {-1,1}, {1,0}},
+	//new int[,] 	{{-1,0}, {0,-1}, {0,-1}, {1,0}, {1,0}, {0,1}, {0,1}, {-1,0}}
+	//	};
+	
 	private int orientationOffset;
-
+	
 	private int[] homeIndex		= new int[2];
 	private int[] cursorIndex	= new int[2];
-
+	
 	private int x;
 	private int z;
-
+	
 	public enum SpaceSelection
 	{
 		Black, 
 		White
 	};
-
+	
 	// Use this for initialization
 	void Start () {
 		pointerSpace = gameObject.name;
 		//Set coordinates for array indexes
 		indexSpace (pointerSpace).CopyTo(homeIndex, 0);
-
-
-		if (SQUARETEST)
-			findSpaces(Movement.MOVEMENTS_AVAILABLE.KNIGHTRIGHT, Monster.MonsterOrientation.North);
+		
+		
+		
+		
+		//if (SQUARETEST)
+		//Debug.Log("Hi, my name is " + gameObject.name + ", and I live at " + homeIndex[0] + ", " + homeIndex[1]);
+		//findSpaces(Movement.MOVEMENTS_AVAILABLE.KNIGHTRIGHT, Monster.MonsterOrientation.North);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-
+		
+		
 		
 	}
-
+	
 	//Direction is put in as the move
 	public string[] findSpaces(Movement.MOVEMENTS_AVAILABLE direction, Monster.MonsterOrientation orientation) {
 		Movement.MOVEMENT_NOTATION[] moveNotation = Movement.findMoves(direction);
-
-		Debug.Log (gameObject.name + ": ");
-
+		
 		string[] moveOrder = new string[moveNotation.Length];
 		int[] indexOrder = new int[moveNotation.Length];
 		int i = 0;
 		homeIndex.CopyTo (cursorIndex, 0);
-
-		try{
+		//Debug.Log("again, my home index is " + homeIndex[0] + ", " + homeIndex[1]);
+		//Debug.Log("My cursor index is " + cursorIndex[0] + ", " + cursorIndex[1]);
+		
+		try {
 			foreach (Movement.MOVEMENT_NOTATION note in moveNotation) {
 				indexOrder = parseNotation(note, orientation);
 				moveOrder[i] = translateVectorIndexToCoordinate(indexOrder);
-				Debug.Log(moveOrder[i]);
-		}
+			}
 		}catch(UnityException e) {
 			//creature attempted to move outside the board boundaries
 		}
-
-
-		return null;
-
-
+		
+		
+		return moveOrder;
+		
+		
 	}
-
+	
 	//Orientation is needed to properly adjust the coordinates from an objective board point of view.
 	//Using that offset, we just rotate thru the possible changes to end up on the one we want
 	//Initially we start on a part of the array, and then later we offset and rotate thru to the desired direction.
 	//A "vectorIndex" is returned. It's a relative space that is applied to another space to calculate the target coordinate.
 	private int[] parseNotation(Movement.MOVEMENT_NOTATION notation, Monster.MonsterOrientation orientation) {
+		
+		//Debug.Log("moving " + notation + " with ori: " + orientation);
 		switch (orientation) {
 		case Monster.MonsterOrientation.North:
 			orientationOffset = 1;
@@ -112,7 +116,7 @@ public class BoardSpace : MonoBehaviour {
 		default:
 			break;
 		}
-
+		
 		switch (notation) {
 		case Movement.MOVEMENT_NOTATION.H:
 			vectorIndex[0] = 0;
@@ -151,9 +155,11 @@ public class BoardSpace : MonoBehaviour {
 			vectorIndex[1] = orientationIndex[cycle(orientationOffset,6),1];
 			break;
 		}
+		
+		//Debug.Log("movement vector = " +vectorIndex[0] + ", " + vectorIndex[1]);
 		return vectorIndex;
 	}
-
+	
 	//Used in cycling thru the possible movement differences
 	private int cycle(int startingPos, int cyclePos) {
 		int tempPos = startingPos + cyclePos;
@@ -163,11 +169,11 @@ public class BoardSpace : MonoBehaviour {
 			return tempPos;
 		}
 	}
-
+	
 	//It's easier to compare number order and index limitations than letters themselves. The home space is stored as array indexes to look up later.
 	private int[] indexSpace(string startingCoordinate) {
 		int[] indexArray = new int[2];
-
+		
 		for (int i = 0; i < boardLetters.Length; i++) {
 			if (startingCoordinate[0].ToString ().Equals (boardLetters [i])) {
 				indexArray[0] = i;
@@ -182,29 +188,33 @@ public class BoardSpace : MonoBehaviour {
 		}
 		return indexArray;
 	}
-
+	
 	private int[] findIndexUsingVectorIndex(int[] vectorIndex){
 		return null;
 	}
-
+	
 	//Converts an x and y coordinate into chessboard coordinate system
 	private string translateIndexSpace(int letterIndex, int numberIndex) {
 		string index = "";
 		index += boardLetters [letterIndex] + "" + boardNumbers [numberIndex];
 		return index;
 	}
-
+	
 	private string translateIndexSpace(int[] indexArray) {
 		return translateIndexSpace (indexArray [0], indexArray [1]);
 	}
-
+	
 	//Letter index first, then number index
 	private string translateVectorIndexToCoordinate(int[] vIndex) {
+		//Debug.Log("Space here, I'm moving from " + cursorIndex[0] + ", " + cursorIndex[1]);
+		
 		cursorIndex [0] = vIndex [0] + cursorIndex [0];
 		cursorIndex [1] = vIndex [1] + cursorIndex [1];
+		
+		//Debug.Log("to " + cursorIndex[0] + ", " + cursorIndex[1]);
 		return translateIndexSpace(cursorIndex);
 	}
-
+	
 	//TODO Out of bounds check
-
+	
 }
--- Assets/Resources/Scripts/Monster.cs
+++ Assets/Resources/Scripts/Monster.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Monster : MonoBehaviour {
-
+	
 	public int power;
 	public int defense;
-	private GameObject homeSpace;
-
+	public MonsterOrientation orientation;
+	public GameObject homeSpace;
+	
+	private MonsterOrientation defaultOrientation;
+	private int orientationIndex;
+	
+	public static MonsterOrientation[] orientationArray = new MonsterOrientation[] { MonsterOrientation.North, MonsterOrientation.NorthEast, MonsterOrientation.East, MonsterOrientation.SouthEast, MonsterOrientation.South, MonsterOrientation.SouthWest, MonsterOrientation.West, MonsterOrientation.NorthWest };
+	
+	
 	//Monster orientation is objective to the board.
 	//Directions are dependent on Orientation.
 	//P1 starts monsters oriented North; P2 starts monsters oriented South.
@@ -21,28 +28,111 @@ public class Monster : MonoBehaviour {
 		West,
 		East
 	};
-
+	
 	private Movement[] MovementOptions;
 	private Movement movesAvailable;
-
+	
 	// Use this for initialization
 	void Start () {
 		if (gameObject.GetComponent<Movement>() != null) {
 			movesAvailable =  gameObject.GetComponent<Movement>();
-
 		}
+		
+		defaultOrientation = MonsterOrientation.North;
+		//IF is player one, default  = north
+		//IF is player two, default = south TODO
+		//IF default is different, make case for change in starting orientation.
+		
+		orientation = defaultOrientation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-
+	
+	//REMOVE AFTER TESTING
+	public void move() {
+		move(Movement.MOVEMENTS_AVAILABLE.FORWARD);
+	}
+	public void move(Movement.MOVEMENTS_AVAILABLE movement) {
+		//Debug.Log("I am here at " + homeSpace.name);
+		string[] moveOrder = new string[10];
+		homeSpace.GetComponent<BoardSpace>().findSpaces(movement, orientation).CopyTo(moveOrder, 0);
+		foreach (string moveSpace in moveOrder) {
+			if (moveSpace != null) {
+				GameObject moveTarget = GameObject.Find(moveSpace);
+				Vector3 tempPos = moveTarget.transform.position;
+				tempPos.y = 0.75f;
+				gameObject.transform.position = tempPos;
+				homeSpace = moveTarget;
+			}
+			
+		}
+		
+		
+	}
+	
 	public GameObject getMovementSpaces() {
 		return null;
 	}
-
+	
 	public GameObject getHomeSpace() {
 		return homeSpace;
 	}
+	
+	//changes orientaiton and returns the result
+	public void rotateClockwise() {
+		int tempOriIndex = orientationIndex + 1;
+		if (tempOriIndex >= orientationArray.Length)
+			tempOriIndex -= orientationArray.Length;
+		
+		setOrientation(tempOriIndex);
+		transform.Rotate(0, transform.rotation.y + 45, 0);
+		//return FindEnumForOrientationFromIndex(tempOriIndex);
+	}
+	
+	public void RotateCounterClockwise() {
+		int tempOriIndex = orientationIndex - 1;
+		if (tempOriIndex < 0)
+			tempOriIndex += orientationArray.Length;
+		
+		setOrientation(tempOriIndex);
+		transform.Rotate(0, transform.rotation.y - 45, 0);
+		//return FindEnumForOrientationFromIndex(tempOriIndex);
+		
+	}
+	
+	public MonsterOrientation setOrientation(int indexOri) {
+		orientation = FindEnumForOrientationFromIndex(indexOri);
+		orientationIndex = indexOri;
+		return orientation;
+	}
+	public MonsterOrientation setOrientation(MonsterOrientation enumOrientation) {
+		orientation = enumOrientation;
+		orientationIndex = FindIndexForOrientationFromEnum(enumOrientation);
+		
+		return defaultOrientation;
+	}
+	
+	public MonsterOrientation getOrientation() {
+		return orientation;
+	}
+	
+	
+	//RECURSIVE WATCH OUT LOL
+	private int FindIndexForOrientationFromEnum(MonsterOrientation findOri) {
+		for(int i = 0; i < orientationArray.Length; i++) {
+			if(findOri == orientationArray[i]) {
+				return i;
+			}
+			return FindIndexForOrientationFromEnum(defaultOrientation);
+		}
+		return 1; //unreachable
+	}
+	
+	private MonsterOrientation FindEnumForOrientationFromIndex(int findOri) {
+		return orientationArray[findOri];
+	}
+	
 }
